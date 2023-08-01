a60d214d0ff7e5497b6324102d257d8130a04221
Dhruv Aggarwal <dhruv.2002agg@gmail.com>
2023-08-01 21:38:54 +0530

    Signing in with MetaTask complete

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const crypto = require('crypto');
+const ethers = require('ethers');
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+app.use(express.static(__dirname));
+const jwt = require('jsonwebtoken');
+app.use(express.json())
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+
+// GET route to retrieve a nonce value for use in signing
+app.get('/api/nonce', (req, res) => {
+  const nonce = crypto.randomBytes(32).toString('hex');
+  res.json({ nonce });
+});
+
+const secretKey = 'ilovecars';
+
+app.post('/login', (req, res) => {
+    const { signedMessage, message, address } = req.body;
+    const recoveredAddress = ethers.utils.verifyMessage(message, signedMessage);
+    if (recoveredAddress !== address) {
+      return res.status(401).json({ error: 'Invalid signature' });
+    }
+    
+    // Generating the JWT token
+    const token = jwt.sign({ address }, secretKey, { expiresIn: '10s' });
+    // Sendind the JWT token to the index.html
+    res.json( token );
+});
+
+
+// Endpoint for verifying the JWT token and logging in the user
+app.post('/verify', (req, res) => {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+    
+    const token = authHeader.split(' ')[1];
+    
+    try {
+      // Verifying the JWT token
+      const decoded = jwt.verify(token, secretKey);
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (decoded.exp < currentTime) {
+        res.json("tokenExpired");
+      } else {
+        res.json("ok");
+      }
+
+      
+    } catch (err) {
+      res.status(401).json({ error: 'Invalid token' });
+    }
+  });
+  
+
+app.get('/success', (req, res) => {
+    res.sendFile(path.join(__dirname + '/success.html'));
+});
+
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
